a2cdb1f57c9467f77d64c6f69c4f45dc8009be08
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:08:41 +0530

    Update notifyController.js

--- Cloud-To-Do/server/controllers/notifyController.js
+++ Cloud-To-Do/server/controllers/notifyController.js
@@ -1,1 +1,23 @@
+const nodemailer = require('nodemailer');
+const User = require('../models/User');
+const Task = require('../models/Task');
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+});
+
+exports.sendReminderEmail = async (req, res) => {
+  const { taskId } = req.body;
+  const task = await Task.findById(taskId).populate('user');
+  if (!task) return res.status(404).json({ msg: 'Task not found' });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: task.user.email,
+    subject: `Reminder: ${task.title}`,
+    html: `<p>Don't forget: <b>${task.title}</b><br>Due: ${task.dueDate || 'No due date'}</p>`
+  });
+
+  res.json({ msg: 'Reminder sent' });
+};
