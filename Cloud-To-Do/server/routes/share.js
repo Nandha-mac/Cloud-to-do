c24ec71bf9879df538e9c41a67e09ff40ebee81b
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:11:39 +0530

    Update share.js

--- Cloud-To-Do/server/routes/share.js
+++ Cloud-To-Do/server/routes/share.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const { shareTask, getSharedTasks, setRole } = require('../controllers/shareController');
+const { authMiddleware } = require('../middleware/auth');
+const router = express.Router();
 
+router.use(authMiddleware);
+
+router.post('/:taskId', shareTask);
+router.get('/', getSharedTasks);
+router.post('/:taskId/role', setRole);
+
+module.exports = router;
