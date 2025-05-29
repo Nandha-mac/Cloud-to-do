31e70521e1400677f066058a1691c6b681381540
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:10:42 +0530

    Update User.js

--- Cloud-To-Do/server/models/User.js
+++ Cloud-To-Do/server/models/User.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose'); // <-- THIS LINE IS REQUIRED
 
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: String, // Not required for OAuth users
+  name: String,
+  oauthProvider: String,
+  roles: { type: [String], default: ['User'] },
+  resetToken: String,
+  resetTokenExpiry: Date
+});
+
+module.exports = mongoose.model('User', userSchema);
