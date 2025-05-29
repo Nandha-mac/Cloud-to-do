17ac0a8201775a3323fd96d1747d43bc860b0fa0
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:11:22 +0530

    Update notify.js

--- Cloud-To-Do/server/routes/notify.js
+++ Cloud-To-Do/server/routes/notify.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const { sendReminderEmail } = require('../controllers/notifyController');
+const { authMiddleware } = require('../middleware/auth');
+const router = express.Router();
 
+router.use(authMiddleware);
+
+router.post('/remind', sendReminderEmail);
+
+module.exports = router;
