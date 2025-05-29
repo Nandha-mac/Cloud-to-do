a0314d4f03b7bbb7ed5f59138eb93ee6a696303b
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:10:27 +0530

    Update Task.js

--- Cloud-To-Do/server/models/Task.js
+++ Cloud-To-Do/server/models/Task.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+const taskSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: { type: String, required: true },
+  description: String,
+  category: String,
+  tags: [String],
+  priority: { type: String, enum: ['High', 'Medium', 'Low'], default: 'Low' },
+  completed: { type: Boolean, default: false },
+  dueDate: Date,
+  sharedWith: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  reminders: [{ type: Date }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
