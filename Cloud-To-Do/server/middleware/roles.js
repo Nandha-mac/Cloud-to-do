5fd97e7bf1a5dd99785e3ae94cda080e80e9a93b
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:10:01 +0530

    Update roles.js

--- Cloud-To-Do/server/middleware/roles.js
+++ Cloud-To-Do/server/middleware/roles.js
@@ -1,1 +1,6 @@
-
+exports.roleMiddleware = (...roles) => (req, res, next) => {
+  if (!roles.some(role => req.user.roles.includes(role))) {
+    return res.status(403).json({ msg: 'Access denied' });
+  }
+  next();
+};
