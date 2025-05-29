5e82914c5dd6bc1916b9a99f8401c8fa4bd348e5
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:08:07 +0530

    Update authController.js

--- Cloud-To-Do/server/controllers/authController.js
+++ Cloud-To-Do/server/controllers/authController.js
@@ -1,1 +1,19 @@
+const signup = async (req, res) => {
+  res.json({msg: "signup works"});
+};
+const login = async (req, res) => {
+  res.json({msg: "login works"});
+};
+const forgotPassword = async (req, res) => {
+  res.json({msg: "forgotPassword works"});
+};
+const resetPassword = async (req, res) => {
+  res.json({msg: "resetPassword works"});
+};
 
+module.exports = {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword
+};
