9becbd46393f91626fe4da94942b26e8f314851e
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:11:56 +0530

    Update task.js

--- Cloud-To-Do/server/routes/task.js
+++ Cloud-To-Do/server/routes/task.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const { getTasks, addTask, updateTask, deleteTask, toggleComplete } = require('../controllers/taskController');
+const { authMiddleware } = require('../middleware/auth');
+const router = express.Router();
 
+router.use(authMiddleware);
+
+// CRUD
+router.get('/', getTasks);
+router.post('/', addTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
+router.put('/:id/complete', toggleComplete);
+
+module.exports = router;
