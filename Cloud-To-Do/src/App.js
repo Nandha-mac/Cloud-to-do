9c1b0c09f4fdd64150b0dff8dbe7ae28c289bd07
Nandha-mac <nandhakumar.p.2024.mecse@rajalakshmi.edu.in>
2025-05-30 05:19:51 +0530

    Update App.js

--- Cloud-To-Do/src/App.js
+++ Cloud-To-Do/src/App.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import theme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import TasksPage from './pages/TasksPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
